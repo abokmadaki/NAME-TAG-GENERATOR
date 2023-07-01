17f2feb618c19722d6907d3a5efac89d7bca8831
Abok Istifanus Madaki <137876407+abokmadaki@users.noreply.github.com>
2023-07-01 10:55:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import NameTagList from "./NameTagList.js";
+import UserInput from "./UserInput.js";
+
+class App extends Component {
+  state = {
+    names: []
+  };
+  addName = (name) => {
+    const newNames = [name, ...this.state.names];
+    this.setState({ names: newNames });
+  };
+  removeName = (clickedIndex) => {
+    const filterCallback = (_, index) => index !== clickedIndex;
+    const newNames = this.state.names.filter(filterCallback);
+    this.setState({ names: newNames });
+  };
+
+  componentDidUpdate() {
+    const savedNamesString = JSON.stringify(this.state.names);
+    localStorage.setItem("savedNames", savedNamesString);
+  }
+
+  componentDidMount() {
+    const savedNamesString = localStorage.getItem("savedNames");
+    if (savedNamesString) {
+      const savedNames = JSON.parse(savedNamesString);
+      this.setState({ names: savedNames });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        <UserInput addName={this.addName} />
+        <NameTagList names={this.state.names} removeName={this.removeName} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/UserInput.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  state = {
+    name: ""
+  };
+  updateName = (event) => this.setState({ name: event.target.value });
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addName(this.state.name);
+    this.setState({ name: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a new name here..."
+          value={this.state.name}
+          onChange={this.updateName}
+        />
+        <input type="submit" value="Create Name Tag" />
+      </form>
+    );
+  }
+}
+
+export default UserInput;
